0bd2a1e06c3439b161ad51c2d9ab399e92be33b4
Sowparna Shome <soupornoshome@gmail.com>
2025-08-22 20:14:06 +0530

    hugging face use as well

--- cluaude_cursor_cli_demo/index.ts
+++ cluaude_cursor_cli_demo/index.ts
@@ -1,15 +1,22 @@
 import { OpenAI } from "openai";
-const api_key =
-  "";
+// const api_key =
+//   "";
+const api_key = "";
+// const client = new OpenAI({
+//   baseURL: "https://openrouter.ai/api/v1",
+//   apiKey: api_key,
+// });
 const client = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
+  baseURL: "https://router.huggingface.co/v1",
   apiKey: api_key,
 });
+// const model = "deepseek/deepseek-r1-0528:free";
+const model = "openai/gpt-oss-20b:fireworks-ai";
 
 async function chatCall(prompt: string) {
     try {
         const completion = await client.chat.completions.create({
-          model: "openai/gpt-oss-20b:free",
+          model: model,
           messages: [
             {
               role: "user",
@@ -17,11 +24,12 @@ async function chatCall(prompt: string) {
             },
           ],
         });
-        console.log(completion?.choices[0]?.message);
+      console.log("total object",completion);
+        console.log("message is",completion?.choices[0]?.message);
     } catch (error) {
         throw Error("Could not recieve a response");
     }
     
 }
 
-chatCall("Who are you?").then().catch(error => console.log(`erorr is ${error}`));
+chatCall("Can you create a package.json file in my pc?").then().catch(error => console.log(`erorr is ${error}`));
