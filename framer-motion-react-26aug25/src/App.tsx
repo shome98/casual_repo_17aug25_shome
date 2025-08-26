df2b2706626e4c7c5a4aeb22404dd8886eff8341
Sowparna Shome <soupornoshome@gmail.com>
2025-08-26 23:58:55 +0530

    test

--- framer-motion-react-26aug25/src/App.tsx
+++ framer-motion-react-26aug25/src/App.tsx
@@ -1,9 +1,21 @@
+import { motion } from "motion/react"
 
 function App() {
-
   return (
     <>
       <h1>Hi</h1>
+      <motion.div
+        animate={{
+          x: 100,
+          rotate:36
+        }}
+        className="box"></motion.div>
+      <motion.div
+        animate={{
+          y:70
+        }}
+        className="circle"
+      ></motion.div>
     </>
   )
 }
