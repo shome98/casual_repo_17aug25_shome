1171f2d5c1536917cbe3256423578f98ef54c42c
Sowparna Shome <soupornoshome@gmail.com>
2025-08-28 01:22:20 +0530

    useref

--- gsap-react-27aug25/src/App.tsx
+++ gsap-react-27aug25/src/App.tsx
@@ -1,37 +1,41 @@
 import { useGSAP } from "@gsap/react";
-import gsap from 'gsap';
+import gsap from "gsap";
 import { useRef } from "react";
 
 function App() {
   const boxref = useRef<HTMLDivElement>(null);
-  useGSAP(() => {
-  //   gsap.from(boxref.current, {
-  //     rotate: 360,
-  //     duration: 1,
-  //     delay:1
-    //  })
-    gsap.from('.box-gradient', {
-      rotate: 3600,
-      scale: 0,
-      duration: 1,
-      opacity: 0,
-      delay:0.5
-    })
-  },{scope:'.container'});
+  const containerRef = useRef<HTMLDivElement>(null);
+  useGSAP(
+    () => {
+      //   gsap.from(boxref.current, {
+      //     rotate: 360,
+      //     duration: 1,
+      //     delay:1
+      //  })
+      gsap.from(".box-gradient", {
+        rotate: 3600,
+        scale: 0,
+        duration: 1,
+        opacity: 0,
+        delay: 0.5,
+      });
+    },
+    { scope:containerRef }
+  );
   return (
     <>
       <main>
-        <div className="container">
-          <div ref={ boxref} className="box-gradient"></div>
+        <div ref={containerRef} className="container">
+          <div ref={boxref} className="box-gradient"></div>
           <div className="circle-gradient"></div>
         </div>
-        <div className="">
+        <div className="container">
           <div className="box-gradient"></div>
           <div className="circle-gradient"></div>
         </div>
       </main>
     </>
-  )
+  );
 }
 
 export default App;
