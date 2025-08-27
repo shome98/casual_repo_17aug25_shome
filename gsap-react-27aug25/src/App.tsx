8aa03846a4e2c110484f236b0e1a33e3d073f98b
Sowparna Shome <soupornoshome@gmail.com>
2025-08-28 01:50:08 +0530

    clean up contextsafe

--- gsap-react-27aug25/src/App.tsx
+++ gsap-react-27aug25/src/App.tsx
@@ -1,40 +1,20 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [x, setX] = useState(0);
-  const [rotate, setRotate] = useState(0);
-  const random = gsap.utils.random(-500, 500, 10);
-  const rotateRandom = gsap.utils.random(-360, 360, 30);
   const boxRef = useRef<HTMLDivElement>(null);
-  const handleAnimate = () => {
-    setX(random);
-    setRotate(rotateRandom);
-  };
-  const xAnimate = () => {
-    gsap.to(boxRef.current, {
-      x: x,
-      duration: 2,
-      rotate: rotate
-    });
-  };
   const { contextSafe } = useGSAP();
   const animate = contextSafe(() => {
     gsap.to(boxRef.current, {
       x: 200,
       duration: 2,
-      rotate: 720
+      rotate: 720,
     });
   });
-    
-  
-   
+
   return (
     <>
-      <button onClick={handleAnimate} className="button-green">
-        Animate
-      </button>
       <button onClick={animate} className="button-green">
         Safely animate
       </button>
