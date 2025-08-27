5138320acf4670efacedf7c93c1f73982ea2e112
Sowparna Shome <soupornoshome@gmail.com>
2025-08-28 00:49:48 +0530

    usegsap

--- gsap-react-27aug25/src/App.tsx
+++ gsap-react-27aug25/src/App.tsx
@@ -1,11 +1,16 @@
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
 
 function App() {
-
+  useGSAP(() => {
+   gsap.to('.box',{x:500,duration:3,delay:1})
+  });
   return (
     <>
       <h1>hi </h1>
+      <div className="box"></div>
     </>
   )
 }
 
-export default App
+export default App;
