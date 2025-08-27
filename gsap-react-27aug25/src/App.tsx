f8e7435507eca226ce68378e526f0e5f0486b38c
Sowparna Shome <soupornoshome@gmail.com>
2025-08-28 00:57:33 +0530

    using useref

--- gsap-react-27aug25/src/App.tsx
+++ gsap-react-27aug25/src/App.tsx
@@ -1,14 +1,16 @@
 import { useGSAP } from "@gsap/react";
 import gsap from 'gsap';
+import { useRef } from "react";
 
 function App() {
+  const gsapref = useRef<HTMLDivElement>(null);
   useGSAP(() => {
-   gsap.to('.box',{x:500,duration:3,delay:1})
+   gsap.to(gsapref.current,{x:500,duration:3,delay:1,rotate:720})
   });
   return (
     <>
       <h1>hi </h1>
-      <div className="box"></div>
+      <div ref={ gsapref} className="box"></div>
     </>
   )
 }
