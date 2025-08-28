9915c2374c9721783b8cd4b8d424b0fd87dcf311
Sowparna Shome <soupornoshome@gmail.com>
2025-08-29 01:02:14 +0530

    check validation

--- first-crud-laravel-28aug25/app/Http/Controllers/UserController.php
+++ first-crud-laravel-28aug25/app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-   public function register(){
+   public function register(Request $request){
+    $incomingFields=$request->validate([
+        'name'=>['required','min:3'],
+        'email'=>['required','email'],
+        'password'=>['required','min:4'],
+    ]);
     return 'Hello from controller';
    }
 }
