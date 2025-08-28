a7977b9435d5744e9f60be253bfb2f1c18393ce6
Sowparna Shome <soupornoshome@gmail.com>
2025-08-29 00:56:41 +0530

    use controller

--- first-crud-laravel-28aug25/routes/web.php
+++ first-crud-laravel-28aug25/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/register',function () {
-    return 'thank you';
-});
+Route::post('/register',[UserController::class,'register']);
