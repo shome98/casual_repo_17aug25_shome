47302ce82cafdd46a79d7692ea5a86c0d5ce2405
Sowparna Shome <soupornoshome@gmail.com>
2025-08-29 19:37:14 +0530

    users posts

--- first-crud-laravel-28aug25/app/Models/User.php
+++ first-crud-laravel-28aug25/app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function usersPosts(){
+        return $this->hasMany(Post::class,'user_id');
+    }
 }
