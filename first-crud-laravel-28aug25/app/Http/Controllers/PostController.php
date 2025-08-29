5402b9e24c5df494bb316be21698a3bb8fce0f43
Sowparna Shome <soupornoshome@gmail.com>
2025-08-29 20:13:09 +0530

    update post

--- first-crud-laravel-28aug25/app/Http/Controllers/PostController.php
+++ first-crud-laravel-28aug25/app/Http/Controllers/PostController.php
@@ -29,4 +29,18 @@ class PostController extends Controller
 
         return view('edit-post', ['post' => $post]);
     }
+    public function updatePost(Post $post, Request $request)
+    {
+        if (auth()->user()->id !== $post['user_id']) {
+            return redirect('/');
+        }
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $post->update($incomingFields);
+        return redirect('/');
+    }
 }
--- first-crud-laravel-28aug25/resources/views/edit-post.blade.php
+++ first-crud-laravel-28aug25/resources/views/edit-post.blade.php
@@ -1,9 +1,9 @@
 <x-layout title="Edit {{$post->id}}">
-        <form action="" method="POST">
+        <form action="/edit-post/{{$post->id}}" method="POST">
             @csrf
             @method('PUT')
             <div class="box-2">
-                <h1 class="text-2xl text-center">Create a Post</h1>
+                <h1 class="text-2xl text-center">Edit {{$post->title}}</h1>
                 <div class="flex-col-2">
                     <label for="title" class="label-normal">Title</label>
                     <input id="title" name="title" type="text" class="input-normal"
