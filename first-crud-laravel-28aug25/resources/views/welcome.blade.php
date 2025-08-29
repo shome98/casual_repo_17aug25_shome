902b9d45c630353be6351c13e2f651a416524a93
Sowparna Shome <soupornoshome@gmail.com>
2025-08-29 19:34:29 +0530

    create post

--- first-crud-laravel-28aug25/resources/views/welcome.blade.php
+++ first-crud-laravel-28aug25/resources/views/welcome.blade.php
@@ -5,6 +5,26 @@
        <form action="/logout" method="POST">
         @csrf
         <button class="button-red">Log out</button></form>
+<br/>
+        <form method="POST" action="/create-post">
+            @csrf
+            <div class="box-2">
+                <h1 class="text-2xl text-center">Create a Post</h1>
+<div class="flex-col-2">
+                <label for="title" class="label-normal">Title</label>
+                <input id="title" name="title" type="text" class="input-normal"
+                    placeholder="Enter a post title..." required>
+            </div>
+            <div class="flex-col-2">
+                <label for="body" class="label-normal">Body</label>
+                <textarea id="body" name="body" type="password" class="input-normal"
+                    placeholder="Enter post details..."></textarea>
+            </div>
+            <button type="submit" class="button-blue-2">
+                Save post
+            </button>
+            </div>
+        </form>
     @else
     <form method="POST" action='/register'>
         @csrf
--- first-crud-laravel-28aug25/routes/web.php
+++ first-crud-laravel-28aug25/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::post('/register',[UserController::class,'register']);
 Route::post('/logout',[UserController::class,'logout']);
 Route::post('/login',[UserController::class,'login']);
+
+Route::post('/create-post',[PostController::class,'createPost']);
